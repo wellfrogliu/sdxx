0ceda4cbf686c8a1ed210ce8dcce212f165c9417
wellfrogliu <woliuxuan1@163.com>
2018-02-26 22:01:36 +0800

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,12 @@
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include "ExtractFeature_.h"
+#include <assert.h>  
+#include <string.h>  
+#include "leveldb/db.h"
+#include <iostream>  
+#include <algorithm>
+#include <iterator>
+#include <fstream>
